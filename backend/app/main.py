df5ad989902b45135f0e8b9d0afa3a2f19831fb3
luiscortesdev <luiscortesdev@gmail.com>
2026-09-05 10:52:12 -0400

    feat(api): configure endpoints and cors

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,11 +1,30 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
-app = FastAPI()
+from app.core.config import settings
+from app.routers.v1.api import api_router
+
+from app import models
+
+app = FastAPI(
+    title=settings.PROJECT_NAME,
+    openapi_url=f"{settings.API_V1_STR}/openapi.json"
+)
+
+# ensure api works in local development
+if settings.BACKEND_CORS_ORIGINS:
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=[str(origin) for origin in settings.BACKEND_CORS_ORIGINS],
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
+
+app.include_router(api_router, prefix=settings.API_V1_STR)
 
 @app.get("/")
-def read_root():
-    return {"Hello": "World"}
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: str | None = None):
-    return {"item_id": item_id, "q": q}
+async def root():
+    return {
+        "message": "Welcome to the Dinerator API"
+    }
