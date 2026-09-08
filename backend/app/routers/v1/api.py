1551efc322d066bbb9929cc2c1c781c82463ab03
luiscortesdev <luiscortesdev@gmail.com>
2026-09-08 00:17:14 -0400

    feat(api): add location endpoint router to api

--- backend/app/routers/v1/api.py
+++ backend/app/routers/v1/api.py
@@ -1,7 +1,8 @@
 from fastapi import APIRouter
-from app.routers.v1 import admin, ratings
+from app.routers.v1 import admin, ratings, location
 
 api_router = APIRouter()
 
 api_router.include_router(admin.router, prefix="/admin", tags=["admin"])
 api_router.include_router(ratings.router, prefix="/ratings", tags=["ratings"])
+api_router.include_router(location.router, prefix="/location", tags=["location"])
